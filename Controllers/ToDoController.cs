d49df832b4c9427900c3ac0ce3162049ffcf8a32
Olawale Lawal <lawale99@gmail.com>
2019-08-23 22:51:07 +0100

    Added ToDo Controller

---
+++ Controllers/ToDoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class ToDoController : Controller
+    {
+        public ToDoController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<ToDo> Get() => null;
+
+        [HttpGet("{id}")]
+        public ToDo Get(int id) => null;
+
+        [HttpPost]
+        public ToDo Post([FromBody] ToDo toDo) => null;
+
+        [HttpPut]
+        public ToDo Put([FromBody] ToDo toDo) => null;
+
+        [HttpDelete("{id}")]
+        public void Delete(int id) { }
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody] JsonPatchDocument<ToDo> jsonPatch) => null;
+    }
+}
