34eb797a6dacb121d22940402b1b68ee68ffca34
Olawale Lawal <lawale99@gmail.com>
2019-08-23 20:06:34 +0100

    Added ToDo and Status Model

---
+++ Models/ToDo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class ToDo
+    {
+        public string Title { get; set; }
+
+        public string Details { get; set; }
+
+        public Status Status { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateCompleted { get; set; }
+    }
+}
